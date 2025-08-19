624a673b5da193ed42c92e3739c02f8dd4f2c596
Charly McIntyre <24mcintyre@gmail.com>
2025-08-19 17:27:05 +1000

    Implemented plotting using minimal cleaned dataset Selection of species using drop-down menu

--- app.R
+++ app.R
@@ -8,21 +8,18 @@ library(leaflet)
 library(sf)
 library(bslib)
 library(ggplot2)
+library(dplyr)
 
 regions <- readRDS("data/processed/regions_sf.rds")
 
-source('R/plot_loader.R')
-
-data <- data.frame(
-  y = c(6, 7, 7, 9, 12, 13, 13, 15, 16, 19, 22, 23, 23, 25, 26),
-  x = c(1, 2, 2, 3, 4, 4, 5, 6, 6, 8, 9, 9, 11, 12, 12)
-)
+# Load data objects
+plot_data <- readRDS("./tests/plot_data2.rds")
 
 # Define UI for application
 ui <- fluidPage(navset_tab(
   nav_panel("Map", page_sidebar(
     sidebar = sidebar(
-      selectizeInput("select", "Choose species:", choices = list('Polioptila_caerulea', 'Chordeiles_minor', 'Anas_platyrhynchos', 'Columba_livia', 'Vireo_gilvus', 'Falco_sparverius', 'Dryocopus_pileatus', 'Hirundo_rustica', 'Sturnella_neglecta', 'Geothlypis_trichas')),
+      selectInput("select", "Choose species:", c("Polioptila_caerulea", "Chordeiles_minor", "Anas_platyrhynchos", "Columba_livia", "Vireo_gilvus", "Falco_sparverius", "Dryocopus_pileatus", "Hirundo_rustica", "Sturnella_neglecta", "Geothlypis_trichas")),
       sliderInput(
         "slider",
         "Year",
@@ -41,6 +38,13 @@ ui <- fluidPage(navset_tab(
 
 # Define server logic
 server <- function(input, output) {
+  # Load data
+  plotDataInput <- reactive({
+    req(input$select)
+    selected_species <- input$select
+    d <- plot_data %>%
+      filter(species == selected_species)
+  })
   output$map <- renderLeaflet({
     leaflet(regions) %>%
       setView(-98, 41, zoom = 4) %>%
@@ -53,10 +57,19 @@ server <- function(input, output) {
       addTiles()
   })
   output$plot <- renderPlot({
-    get_plot(1)
-  })
-  
+    ggplot(plotDataInput(), aes(x = year, y = pred)) +
+      geom_line(linewidth = 1, alpha = 0.7) +
+      geom_ribbon(aes(ymin = lower, ymax = upper), alpha = 0.3) +
+      theme_classic()
+  },
+  width = 200,
+  height = 200
+  )
+
+
 }
+
+
 
 # Run the application
 shinyApp(ui = ui, server = server)
